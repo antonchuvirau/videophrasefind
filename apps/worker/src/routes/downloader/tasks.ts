945ccf73f22e60393ddadeffd7d3f2ec9d5f1ffc
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-05-06 16:22:18 +0300

    feat(worker): add commented code snippets for future use

--- apps/worker/src/routes/downloader/tasks.ts
+++ apps/worker/src/routes/downloader/tasks.ts
@@ -5,14 +5,15 @@ import { engine } from "../../twelveLabs/engines";
 
 import { getS3DirectoryUrl } from "../../lib/s3";
 
+// const MAX_ALLOWED_SECONDS = 300;
+
 export async function trigger12LabsTask({ videoId }: { videoId: string }) {
-  console.log("Triggering ", {
-    url: `${getS3DirectoryUrl(videoId)}/video.webm`,
-    videoId,
-  });
+  console.log(`Triggering 12Labs task for: ${{ videoId }}`);
+
+  // const duration = await getVideoDurationInSeconds(`${getS3DirectoryUrl(videoId)}/video.webm`)
 
   const index = await client12Labs.index.create({
-    name: videoId,
+    name: videoId, // = `${duration > MAX_ALLOWED_SECONDS ? "full" : "cropped"}.${videoId}`;
     engines: engine,
     addons: ["thumbnail"],
   });
@@ -27,6 +28,20 @@ export async function trigger12LabsTask({ videoId }: { videoId: string }) {
       twelveLabsIndexId: index.id,
     },
   });
+
+  // if (duration > MAX_ALLOWED_SECONDS) {
+  //   await cropAndUpload(videoId);
+
+  //   await client12Labs.task.create({
+  //     indexId: index.id,
+  //     url: `${getS3DirectoryUrl(videoId)}/video.cropped.webm`,
+  //   });
+  // } else {
+  //   await client12Labs.task.create({
+  //     indexId: index.id,
+  //     url: `${getS3DirectoryUrl(videoId)}/video.webm`,
+  //   });
+  // }
 
   const task = await client12Labs.task.create({
     indexId: index.id,
